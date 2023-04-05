acda571f372950ef1f7f0286b32bffed7090b5f3
leslieshea <lshea@audigy.com>
2023-04-05 16:34:34 -0700

    Built out navbar with img and titles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import reactLogo from '../images/react-icon-small.png';
 
 export default function Navbar() {
-	return <h1>Navbar goes here</h1>;
+	return (
+		<nav>
+			<img
+				src={reactLogo}
+				alt="React Logo"
+				className="nav--icon"
+			/>
+			<h3 className="nav--logo_text">ReactFacts</h3>
+			<h4 className="nav--title">React Course - Project 1</h4>
+		</nav>
+	)
 }
